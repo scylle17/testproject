3b5f692b65a99f145ba5eeef27997679cacb26d7
Crisselle Mercado <v-crmerc@microsoft.com>
2020-03-30 14:52:58 -0700

    update our menu-items to navigate withRouter()

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -40,15 +40,16 @@ class Directory extends React.Component {
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
-        }]
+          }
+        ]
       };
     }
 
     render() {
       return (
         <div className='directory-menu'>
-          {this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          {this.state.sections.map(({id, ...othersectionProps }) => (
+            <MenuItem key={id} {...othersectionProps}/>
           ))}
         </div>
       );
